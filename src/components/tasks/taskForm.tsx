f033520e90a961941aa2be46a1960860bb90b76f
alee279 <amlee424@gmail.com>
2024-04-11 16:14:54 -0400

    feat: task form

--- src/components/tasks/taskForm.tsx
+++ src/components/tasks/taskForm.tsx
@@ -1,2 +1,29 @@
-// form used to create new tasks or edit existing ones
-export {}
+import { useState } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+import Task from "../types";
+
+interface IFormInput {
+  taskName: string;
+  dailyGoal: number;
+}
+
+const TaskForm = () => {
+  const { register, handleSubmit, reset } = useForm<IFormInput>(); // Specify the type for useForm
+  const [data, setData] = useState("");
+
+  const onSubmit: SubmitHandler<IFormInput> = (formData) => {
+    console.log(formData);
+    reset();
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input {...register("taskName")} placeholder="Task Name" />
+      <input {...register("dailyGoal")} placeholder="Daily Goal" />
+      <p>{data}</p>
+      <input type="submit" />
+    </form>
+  );
+};
+
+export default TaskForm;
--- src/components/tasks/taskList.tsx
+++ src/components/tasks/taskList.tsx
@@ -1,11 +1,13 @@
 // display container of tasks + collapsible task form for new ones
 
 import Task from "./task";
+import TaskForm from "./taskForm";
 
 const TaskList = () => {
   return (
     <>
       <Task />
+      <TaskForm />
     </>
   );
 };
